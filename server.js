153392a5fba06a6155e012a006c28dc07af65177
Eleonora <eleonora.avaleva@gmail.com>
2025-03-25 23:23:40 +0100

    Adds swagger configuration

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 require('dotenv').config();
 
+// Set environment
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 8000;
+
+// Create Express application
 const app = express();
 
 const loaders = require("./src/loaders");
 
-const PORT = process.env.PORT || 8000;
-
 async function startServer() {
-    // Init application loaders
     try {
+        // Initialize all application loaders
         await loaders(app);
+        console.log(`App initialized successfully in ${NODE_ENV} mode`);
 
-        console.log('App initialized successfully');
         // Start server
         app.listen(PORT, () => {
-            console.log(`Server listening on PORT ${PORT}`);
-        });
+            console.log(`${NODE_ENV} mode     ||  Local:   http://localhost:${PORT}     || API Docs: http://localhost:${PORT}`);});
     }
     catch (error) {
         console.error('Failed to start server:', error);
         process.exit(1);
     }
 }
-
+// Start the server
 startServer();
